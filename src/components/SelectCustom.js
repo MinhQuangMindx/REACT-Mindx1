5c498158a562fb8dcdb3d98363dd4665e9708dc1
MinhQuangMindx <vincentequang97@gmail.com>
2023-05-28 22:12:56 +0700

    Create SelectCustom.js

---
+++ src/components/SelectCustom.js
@@ -0,0 +1,24 @@
+// const SelectCustom = ({ name, id, data }) => {
+//   return (
+//     <select
+//       name={name}
+//       id={id}
+//       style={{
+//         width: "100%",
+//         height: 40,
+//         borderRadius: 5,
+//         borderColor: "GrayText",
+//       }}
+//     >
+//       {data.map((item, index) => {
+//         return (
+//           <option key={index} value={item?.value}>
+//             {item?.label}
+//           </option>
+//         );
+//       })}
+//     </select>
+//   );
+// };
+
+// export default SelectCustom;
