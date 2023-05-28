3d42d2867de0051fbcbc9b0b8ab9792efbfee6c5
MinhQuangMindx <vincentequang97@gmail.com>
2023-05-28 22:13:55 +0700

    Create InputRadio.js

---
+++ src/components/InputRadio.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const RadioInput = ({ title, name, dataRadio, onChange, value }) => {
+  console.log(value);
+  return (
+    <div className="mb-3">
+      <label for="formGroupExampleInput" class="form-label">
+        {title}
+      </label>
+      <div style={{ marginLeft: 20, paddingBottom: 12 }}>
+        {dataRadio.map((item, index) => (
+          <div key={index} className="form-check  ">
+            <label className="form-check-label">{item.label}</label>
+            <input
+              className="form-check-input"
+              type="radio"
+              name={name}
+              value={item.key}
+              checked={item.key == value ? true : false}
+              onChange={onChange}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RadioInput;
