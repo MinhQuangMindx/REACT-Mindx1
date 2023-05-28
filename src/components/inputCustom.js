b9eb5d6c7a9b0cdf55d73f6bbe88c3fababc8f0c
MinhQuangMindx <vincentequang97@gmail.com>
2023-05-28 22:13:52 +0700

    Create inputCustom.js

---
+++ src/components/inputCustom.js
@@ -0,0 +1,99 @@
+// import React from "react";
+
+// import { createTheme } from "@mui/material/styles";
+// import blue from "@mui/material/colors/blue";
+
+// // const Theme = createTheme({
+// //   palette: {
+// //     primary: blue,
+// //   },
+// // });
+
+// function Card(props) {
+//   return (
+//     <div ClassName="container">
+//       <div ClassName="img"> {props.img} </div>
+//       <div ClassName="title">
+//         Nhân dân tệ xuống thấp nhất 5 tháng so với USD {props.title}
+//       </div>
+//       <div ClassName="content">
+//         Chiều nay, giá nhân dân tệ trên thị trường quốc tế giảm 0.2% so với USD,
+//         thấp nhất 5 tháng qua, do số liệu kinh tế Trung quốc tuần nầ kém{" "}
+//         {props.content}
+//       </div>
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(
+//   <div>
+//     <Card img="src=1" title="" content=""></Card>
+//   </div>,
+//   document.getElementById("app")
+// );
+
+// {
+//   /* const InputCustom = ({ type, name, placeholder, value }) => {
+//   const styleInput = {
+//     inputContainer: {
+//       width: "100%",
+//       height: 40,
+//       borderRadius: 5,
+//       borderColor: "GrayText",
+//     },
+//     text: {
+//       fonrSize: 16,
+//     },
+//   };
+
+//   return type == "text" ? (
+//     <input
+//       type={type}
+//       name={name}
+//       placeholder={placeholder}
+//       style={styleInput.inputContainer}
+//       value={value}
+//     />
+//   ) : type == "radio" ? (
+//     <input
+//       type={type}
+//       name={name}
+//       placeholder={placeholder}
+//       style={{
+//         width: 16,
+//         height: 16,
+//       }}
+//       value={value}
+//     />
+//   ) : (
+//     <input type={type} name={name} placeholder={placeholder} value={value} />
+//   );
+// // }; */
+// // }
+// // export default Card ;
+
+// // function Avatar() {
+// //   return (
+// //     <div ClassName="Container">
+// //       <img
+// //       className="avatar"
+// //       src="https://khomohinh.com/wp-content/uploads/2022/12/monkey-d-luffy-sinh-ngay-may-tong-hop-phim-hoat-hinh-luffy-moi-nhat-1.jpg"
+// //       alt="Lin Lanying"
+// //       h1="Nhân dân tệ xuống thấp nhất 5 tháng so với USD"
+// //       p="Chiều nay, giá nhân dân tệ trên thị trường quốc tế giảm 0.2% so với USD,
+// //       thấp nhất 5 tháng qua, do số liệu kinh tế Trung quốc tuần nầ kém"
+// //       width={300}
+// //       height={300}></img>
+// //     </div>
+
+// //     <div>
+
+// // //     </div>
+// //   );
+// // }
+
+// // export default function Profile() {
+// //   return (
+// //     <Avatar />
+// //   );
+// // }
