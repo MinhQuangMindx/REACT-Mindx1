a4732f241ba01c059d8faca5d29360b65c8b0e00
MinhQuangMindx <vincentequang97@gmail.com>
2023-05-28 22:13:57 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,113 @@
+// import logo from './logo.svg';
+import "./App.css";
+import FormS from "./FormInput/FormInput";
+function App() {
+  return <FormS />;
+}
+export default App;
+// import "./components/inputCustom";
+// import InputCustom from "./components/inputCustom";
+// import "./components/SelectCustom";
+// import Card from "./components/card";
+
+//MUI
+// Lession2
+// function App() {
+//   return (
+//     <div class="main">
+
+//       <p class="text-about"> Student Registeration Form </p>
+//       <div class="header">
+//         <div class="header-name">
+//           <form action="">
+//             <p>Firstname</p>
+//             <InputCustom
+//               type="text"
+//               placeholder="Firstname"
+//               class="firstname"
+//             />
+
+//             <p>Middlename:</p>
+//             <InputCustom
+//               type="text"
+//               placeholder="Middlename"
+//               class="Middlename"
+//             />
+
+//             <p>Lastname:</p>
+//             <input type="text" placeholder="Lastname" class="Lastname"></input>
+//           </form>
+//         </div>
+
+//         <div class="header-course">
+//           <label for="Course-choice" class="course">
+//             Course :
+//           </label>
+//           {/* <input list="course" id="Course-choice" placeholder="Course"> */}
+//           {/* <datalist id="course">
+//                     <option value="HTML">
+//                     <option value="CSS">
+//                     <option value="Javascript">
+//                     <option value="Python">
+//                 </datalist> */}
+//         </div>
+
+//         <div class="slider">
+//           <div class="text">
+//             <form action="">
+//               <div class="gender">
+//                 <p>Gender :</p>
+//                 <InputCustom type="radio" name="Sex" value="Male" />
+//                 <span>Male</span>
+//                 <InputCustom type="radio" name="Sex" value="Female" />
+//                 <span>Female</span>
+//                 <InputCustom type="radio" name="Sex" value="Other" />
+//                 <span>Other</span>
+//                 {/* <input type="radio" name="Sex" value="Male"></input>
+//                 <input type="radio" name="Sex" value="Female"></input>
+//                 <input type="radio" name="Sex" value="Other"></input> */}
+//               </div>
+//               <p>Phone :</p>
+//               <input type="tel" value="+91" class="phone-1"></input>
+//               <input type="tel" placeholder="Phone no." class="phone-2"></input>
+//               <p>Current Address :</p>
+//               <input
+//                 type="text"
+//                 placeholder="Current Address"
+//                 class="address"
+//               ></input>
+//             </form>
+//           </div>
+//         </div>
+//         <div class="content">
+//           <form action="">
+//             <b>Email</b>
+//             <input type="email" placeholder="Enter Email" class="email"></input>
+
+//             <b>Password</b>
+//             <input
+//               type="password"
+//               placeholder="Enter Password"
+//               class="password"
+//             ></input>
+
+//             <b>Re-type Password</b>
+//             <input
+//               type="password"
+//               placeholder="Retype Password"
+//               class="repassword"
+//             ></input>
+//           </form>
+//         </div>
+//         <div class="footer">
+//           <p class="register">Registeration</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+// Lession2
+
+// Lession 3
+
+// export default App;
